79969cde4501fc6c0397ac4a67df0189b6be5773
mokshdigital <aakash@mokshdigital.com>
2025-12-23 10:55:24 -0800

    Fix build: Remove OfficeStaffForm

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -7,8 +7,7 @@ export type { EquipmentFormData } from './EquipmentForm';
 export { VehicleForm } from './VehicleForm';
 export type { VehicleFormData } from './VehicleForm';
 
-export { OfficeStaffForm } from './OfficeStaffForm';
-export type { OfficeStaffFormData } from './OfficeStaffForm';
+
 
 export { ClientForm } from './ClientForm';
 export type { ClientFormData } from './ClientForm';
